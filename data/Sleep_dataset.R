02bb6e0b53ecf976e42b3539432fafb9a23d12f3
viol1968 <318256@stud.umk.pl>
2024-01-27 23:32:23 +0100

    Imported Sleep_dataset

---
+++ data/Sleep_dataset.R
@@ -0,0 +1,2 @@
+url <- "https://raw.githubusercontent.com/IS-UMK/mp-2023-ds-project-StowarzyszenieOvi/master/data/Sleep_dataset.csv?token=GHSAT0AAAAAACNMDJWC6XF3RAOH55D7QKXIZNVQ6OQ"
+Sleep_dataset <- read.csv(url)
